99058d243d5077bbc25960ee0bf800fc6d1a42e2
Bamboudorer <lucas.techer@epitech.eu>
2022-03-04 02:51:08 +0400

    Add a SingleChildScrollView in the HomePage

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -11,25 +11,27 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // margin: const EdgeInsets.all(15),
-      // padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
-      // height: 220,
-      // constraints: const BoxConstraints.expand(height: 220),
-      // decoration: BoxDecoration(
-      //   borderRadius: BorderRadius.circular(15),
-      //   color: const Color.fromARGB(255, 196, 195, 195),
-      // ),
-      // child: Text("HOME PAGE"),
-      child: Column (
-        children: [
-          Text("HOME PAGE"),
-          HomeBalance(),
-          HomeExpense(),
-          HomeConvertion(),
-          HomeWallet(),
-          // HomeChart(title: "Chart"),
-        ],
+    return SingleChildScrollView(
+      child: Container(
+        // margin: const EdgeInsets.all(15),
+        // padding: const EdgeInsets.only(left: 20, right: 20, top: 10, bottom: 10),
+        // height: 220,
+        // constraints: const BoxConstraints.expand(height: 220),
+        // decoration: BoxDecoration(
+        //   borderRadius: BorderRadius.circular(15),
+        //   color: const Color.fromARGB(255, 196, 195, 195),
+        // ),
+        // child: Text("HOME PAGE"),
+        child: Column (
+          children: [
+            Text("HOME PAGE"),
+            HomeBalance(),
+            HomeExpense(),
+            HomeConvertion(),
+            HomeWallet(),
+            // HomeChart(title: "Chart"),
+          ],
+        ),
       ),
     );
   }
